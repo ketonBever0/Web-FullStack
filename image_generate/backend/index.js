07e9d860a68dc323b551706a65ae67b010e41d0d
ketonBever0 <lacikurucz09@gmail.com>
2023-02-05 14:31:59 +0100

    Update index.js

--- image_generate/backend/index.js
+++ image_generate/backend/index.js
@@ -11,12 +11,12 @@ app.use(express.urlencoded({ extended: false }));
 
 app.listen(process.env.PORT || 8000, () => console.log("Running!"));
 
-app.use('/api', require('./routes/imageGenerateR'))
+app.use('/api', require('./routes/imageGenerateR'));
 
 
 app.get('/', (req, res) => {
     res.send("<h2>Image Generate API</h2>")
-})
+});
 
 
 
