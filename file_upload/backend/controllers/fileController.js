72d447be924e5b11e21bd7821396a71047092e9d
Kurucz Laci <lacikurucz09@gmail.com>
2024-11-27 20:22:07 +0100

    Multer doesn't work in Express

--- file_upload/backend/controllers/fileController.js
+++ file_upload/backend/controllers/fileController.js
@@ -1,119 +1,105 @@
-var fs = require('fs');
-const { dirname } = require('path');
+const fs = require("fs");
+const { dirname } = require("path");
 const appDir = dirname(require.main.filename);
-const mysql = require('mysql');
+const mysql = require("mysql");
 const db = mysql.createConnection({
-    "host": "localhost",
-    "user": "root",
-    "password": "",
-    "database": "file_upload"
+	host: "localhost",
+	user: "root",
+	password: "",
+	database: "file_upload",
 });
 
-
 const fetchFile = (req, res) => {
-    db.query(
-        `
+	db.query(
+		`
             SELECT path
             FROM files
-            WHERE path=?            
+            WHERE path=?
         `,
-        [req.params.path],
-        (err, rows) => {
-            if (err) res.status(400).send(err);
-            res.sendFile(`${appDir}/uploads/${rows[0].path}`);
-        }
-    )
-}
-
-
+		[req.params.path],
+		(err, rows) => {
+			if (err) res.status(400).send(err);
+			res.sendFile(`${appDir}/uploads/${rows[0].path}`);
+		}
+	);
+};
 
 const getFiles = (req, res) => {
-    db.query(
-        `
+	db.query(
+		`
             SELECT *
                 FROM files
 
                 `,
-        [],
-        (err, rows) => {
-            if (err) res.status(400).send(err);
-            res.json(rows);
-        }
-    )
-}
-
+		[],
+		(err, rows) => {
+			if (err) res.status(400).send(err);
+			res.json(rows);
+		}
+	);
+};
 
 const uploadFile = (req, res) => {
-    console.log(req.files.file);
-    // console.log(req.files);
+	// console.log(req.files.file);
+	// console.log(req.files);  
 
-    var dir = appDir + "/uploads/";
-    if (!fs.existsSync(dir)) {
-        fs.mkdirSync(dir, { recursive: true });
-    }
+	const filePaths = req.files.map((file) => file.path);
 
+	// files.forEach((file) => {
+	// 	if (queryStr != "") queryStr += ", ";
+	// 	const fileName = Date.now() + "_" + req.files.file.name;
+	// 	var filePath = appDir + "/uploads/";
+	// 	file.mv(filePath + fileName);
+	// 	queryStr += `("${fileName}")`;
+	// });
 
-
-    const file = req.files.file
-    const fileName = Date.now() + "_" + req.body.fileName;
-
-    var filePath = appDir + "/uploads/";
-
-    console.log(req.body);
-    file.mv(filePath + fileName, (err) => {
-        if (err) console.log(err)
-        else {
-            db.query(
-                `
-                    INSERT INTO files
-                    SET path =?
-                `,
-                [fileName],
-                (err, rows) => {
-                    if (err) return res.status(400).send(err);
-                    else return res.status(200).json(rows);
-                }
-            )
-        }
-
-    })
-
-}
-
+	const rows = db.query(
+		`
+            INSERT INTO files (path)
+            VALUES ?;
+        `,
+		[filePaths.map(path => [path])],
+		(err) => {
+			if (err) return res.status(400).send(err);
+		}
+	);
+	console.log(rows);
+	return res.status(200).json(rows);
+};
 
 const deleteFile = (req, res) => {
-    var dir = appDir + "/uploads/";
+	const file = req.body.path;
+	console.log(req.body);
 
-    const file = req.body.path;
-    console.log(req.body);
-
-    file && fs.unlink(dir + file, (err) => {
-        if (err) {
-            return res.status(400).json({ message: "Hiba!" });
-        } else {
-            db.query(`
+	file &&
+		fs.unlink(dir + file, (err) => {
+			if (err) {
+				return res.status(400).json({ message: "Hiba!" });
+			} else {
+				db.query(
+					`
             DELETE FROM files
             WHERE path=?
             `,
-                [file],
-                (err, rows) => {
-                    if (err) return res.status(400).send(err);
-                    else return res.status(200).json({ message: "Törölve!", affectedCount: rows.affectedCount });
-                }
-            )
-        }
-    })
+					[file],
+					(err, rows) => {
+						if (err) return res.status(400).send(err);
+						else
+							return res.status(200).json({
+								message: "Törölve!",
+								affectedCount: rows.affectedCount,
+							});
+					}
+				);
+			}
+		});
 
-    // return res.status(400).json({ message: "Hiba!" });
-
-
-}
-
-
+	// return res.status(400).json({ message: "Hiba!" });
+};
 
 module.exports = {
-    fetchFile,
-    getFiles,
-    uploadFile,
-    deleteFile
-}
+	fetchFile,
+	getFiles,
+	uploadFile,
+	deleteFile,
+};
--- file_upload/frontend/src/components/context/fileContext.jsx
+++ file_upload/frontend/src/components/context/fileContext.jsx
@@ -1,93 +1,110 @@
-import axios from 'axios';
-import React, { useState, useEffect, createContext } from 'react';
-import Notify from '../../allUse/Toasts';
-
+import axios from "axios";
+import React, { useState, useEffect, createContext } from "react";
+import Notify from "../../allUse/Toasts";
 
 const FileContext = createContext();
 
-
 export const FileProvider = ({ children }) => {
+	const [refresh, setRefresh] = useState(false);
+	const [isLoading, setIsLoading] = useState(false);
 
-    const [refresh, setRefresh] = useState(false);
-    const [isLoading, setIsLoading] = useState(false);
+	const [files, setFiles] = useState(null);
 
-    const [files, setFiles] = useState(null);
+    
+	// Egy fájl esetén
+	// const [inputFile, setInputFile] = useState(null);
+	
+    // Átírtam úgy, hogy több fájlt is lehessen egyszerre
+    const [inputFiles, setInputFiles] = useState(null);
 
-    const [inputFile, setInputFile] = useState(null);
+	const update = () => setRefresh((prev) => !prev);
 
+	const fetchFiles = async () => {
+		setIsLoading(true);
+		// fetch('http://localhost:8000/api/files')
+		//     .then(res => res.json())
+		//     .then(data => setFiles(data))
+		//     .catch(err => console.log(err));
 
-    const update = () => setRefresh(prev => !prev);
+		await axios
+			.get("http://127.0.0.1:8000/api/files")
+			.then((res) => setFiles(res.data));
+		setIsLoading(false);
+	};
 
-    const fetchFiles = async () => {
-        setIsLoading(true);
-        // fetch('http://localhost:8000/api/files')
-        //     .then(res => res.json())
-        //     .then(data => setFiles(data))
-        //     .catch(err => console.log(err));
+	const uploadFile = async () => {
 
-        await axios.get('http://127.0.0.1:8000/api/files')
-            .then(res => setFiles(res.data));
-        setIsLoading(false);
-    }
+		//  A FormData egy olyan JavaScript-beli objektum, ami az űrlap feldolgozást könnyíti
+		// - támogat fájlküldést, ahogy alább van használva
+		const fileForm = new FormData();
+		// hozzáadjuk a fájlokat "files" kulcsként
+		for (let i = 0; i < inputFiles.length; i++) {
+			fileForm.append("files", inputFiles[i]);
+		}
+		// console.log(fileForm.getAll("files"));
 
-    const uploadFile = async (file) => {
-        // console.log(file);
+		// ha egyéb (pl. űrlap) adatokat akarsz küldeni, hozzáappendelheted pl. "xyform" kulcsnéven
 
-        const fileForm = new FormData();
-        fileForm.append("message", "Szia")
-        fileForm.append("file", file);
-        fileForm.append("fileName", file.name);
+		// Axios post-nál második paraméter az elküldendő adat, de itt nem "application/json" lesz
+		await axios
+			.post("http://localhost:8000/api/files", fileForm, {
+				headers: {
+					// "Content-Type": "multipart/form-data", //  Ezzel beállítjuk a tartalom típusát
+				},
+			})
+			.then(Notify.tSuccess("Feltöltve!")) // ez mind1, csak egy üzenetet kapsz
+			.catch((err) => console.log(err));
 
-        for (var i of fileForm.values()) {
-            console.log(i)
-        }
+		// fetch()-et felejtsétek el! Axios jobb!
+		// await fetch("http://localhost:8000/api/files", {
+		// 	method: "POST",
+		// 	body: {
+		// 		files: {
+		// 			file: file,
+		// 		},
+		// 	},
+		// 	headers: {
+		// 		"content-type": file.type,
+		// 		"content-length": `${file.size}`,
+		// 	},
+		// })
+		// 	.then((res) => res.json())
+		// 	.then((data) => setFiles(data))
+		// 	.catch((err) => console.log(err));
 
-        await axios.post('http://localhost:8000/api/files', fileForm, {
-            headers: {
-                'Content-Type': 'multipart/form-data'
-            }
-        })
-            .then(Notify.tSuccess("Feltöltve!"))
-            .catch(err => console.log(err));
+		//  Frissítés függvény újra lekérdezi az adatbázist
+		update();
+	};
 
+	const deleteFile = async (path) => {
+		await axios
+			.delete(`http://127.0.0.1:8000/api/files/delete`, {
+				data: { path: path },
+			})
+			.then((res) => Notify.tSuccess(res.data.message));
+		update();
+	};
 
-        // await fetch('http://localhost:8000/api/files', {
-        //     method: 'POST',
-        //     body: {
-        //         files: {
-        //             file: file
-        //         }
-        //     },
-        //     headers: {
-        //         'content-type': file.type,
-        //         'content-length': `${file.size}`
-        //     }
-        // })
-        //     .then(res => res.json())
-        //     .then(data => setFiles(data))
-        //     .catch(err => console.log(err));
-
-        update();
-
-    }
-
-    const deleteFile = async (path) => {
-        await axios.delete(`http://127.0.0.1:8000/api/files/delete`, { data: { path: path } })
-            .then(res => Notify.tSuccess(res.data.message));
-        update();
-    }
-
-
-
-
-
-    return <FileContext.Provider value={{
-        refresh, update,
-        isLoading,
-        fetchFiles, uploadFile, deleteFile,
-        files, setFiles,
-        inputFile, setInputFile
-    }}>{children}</FileContext.Provider>
-}
+	return (
+		<FileContext.Provider
+			value={{
+				refresh,
+				update,
+				isLoading,
+				fetchFiles,
+				uploadFile,
+				deleteFile,
+				files,
+				setFiles,
+				// inputFile,
+				// setInputFile,
+                inputFiles,
+				setInputFiles,
+			}}
+		>
+			{children}
+		</FileContext.Provider>
+	);
+};
 
 export default FileContext;
